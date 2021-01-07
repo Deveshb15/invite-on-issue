7daac7d31b9110ba37651558932f7bf33db947a3
vj-abishek <abigosearch@gmail.com>
2021-01-07 16:46:34 +0530

    Fixed #1

--- index.js
+++ index.js
@@ -5,19 +5,25 @@ const { ACCESS_TOKEN } = process.env
 const main = async () => {
     try {
         const octokit = new github.GitHub(ACCESS_TOKEN)
-        const payload = github.context.payload
-        const label = payload.label.name
-        const invitee_id = payload.issue.user.id
-        const org = "EddieJaoudeCommunity"
 
-        if (label === "bug") // or invite me to the organisation
-        {
-            await octokit.orgs.createInvitation({
-                org,
-                invitee_id
-            })
-            console.log("Successfully sent invitation")
+        const payload = github.context.payload
+        const invitee_id = payload.issue.user.id
+        const currentLabel = payload.label.name
+
+        const org = core.getInput('organization', { required: true })
+        const label = core.getInput('label', { required: true })
+
+        if (currentLabel === label) {
+            console.log('testing, both are same. Invitation can be sent')
+            console.log(invitee_id, org)
+            console.log("label:", label, "invite:", invitee_id)
+            // await octokit.orgs.createInvitation({
+            //     org,
+            //     invitee_id
+            // })
+            // console.log("Successfully sent invitation")
         }
+
     } catch (error) {
         core.setFailed(error.message);
     }
